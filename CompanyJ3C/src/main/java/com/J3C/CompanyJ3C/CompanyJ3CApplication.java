fe1fc3d1a2777348faf10ac1b7d2dfa7e9e4ee95
Jonathan <jbf2236@gmail.com>
2022-08-24 20:53:43 -0500

    hola a todos

--- CompanyJ3C/src/main/java/com/J3C/CompanyJ3C/CompanyJ3CApplication.java
+++ CompanyJ3C/src/main/java/com/J3C/CompanyJ3C/CompanyJ3CApplication.java
@@ -15,7 +15,7 @@ public class CompanyJ3CApplication {
 		SpringApplication.run(CompanyJ3CApplication.class, args);
 	}
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "name", defaultValue = "todos") String name) {
+		return String.format("Hola a %s!", name);
 	}
 }
