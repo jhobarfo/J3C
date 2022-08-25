f56a1d81676d34aba6995791026e4f4b912e9434
Jonathan <jbf2236@gmail.com>
2022-08-24 20:46:40 -0500

    Hola Mundo

--- CompanyJ3C/src/main/java/com/J3C/CompanyJ3C/CompanyJ3CApplication.java
+++ CompanyJ3C/src/main/java/com/J3C/CompanyJ3C/CompanyJ3CApplication.java
@@ -2,12 +2,20 @@ package com.J3C.CompanyJ3C;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class CompanyJ3CApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyJ3CApplication.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
