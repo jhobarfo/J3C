2c59ebcc1d1a74e23779a693cfef6003e00bab2d
Jonathan <jbf2236@gmail.com>
2022-08-24 20:16:06 -0500

    Creación cascaron para proyecto

---
+++ CompanyJ3C/src/main/java/com/J3C/CompanyJ3C/CompanyJ3CApplication.java
@@ -0,0 +1,13 @@
+package com.J3C.CompanyJ3C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyJ3CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyJ3CApplication.class, args);
+	}
+
+}
